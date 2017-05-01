8095341045af063deba80c5f6a5d43e42d540f39
Ali <aliroshan@live.com>
2017-05-01 15:03:26 +0500

    added page render time in footer area

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -15,6 +15,11 @@
 	                    <p>Copyright &copy; BigOSol.com 2017</p>
 	                </div>
 	            </div>
+	            <div class="row">
+	                <div class="col-lg-12">
+	                    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	                </div>
+	            </div>
 	        </footer>
 
 	    </div>
